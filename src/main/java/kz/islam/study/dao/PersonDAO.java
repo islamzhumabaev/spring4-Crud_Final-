324cfbead0fc44ec84876f38e49ab06f93467a92
islamzhumabaev <129617690+islamzhumabaev@users.noreply.github.com>
2024-07-10 15:40:19 +0500

    Add files via upload

---
+++ src/main/java/kz/islam/study/dao/PersonDAO.java
@@ -0,0 +1,45 @@
+package kz.islam.study.dao;
+
+import kz.islam.study.models.Person;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    public PersonDAO(){
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        for (int i = 0; i < people.size(); i++) {
+            if(people.get(i).getId() == id){
+                return people.get(i);
+            }
+        }
+        return null;
+    }
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+    public void update(int id, Person UpdatedPerson) {
+        Person ToBeUpdatedPerson = show(id);
+        ToBeUpdatedPerson.setName(UpdatedPerson.getName());
+    }
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
+}
